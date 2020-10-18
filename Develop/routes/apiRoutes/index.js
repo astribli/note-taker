7f5dfe5b9323b3341cd301ad5c862f71daeeff3d
astribli <andrewjstribling@gmail.com>
2020-10-17 23:51:53 -0500

    minor edits

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
+const { notes } = require('../../db/db.json');
 
-router.get
+router.get("/notes", (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+    req.body = notes;
+    res.json(notes);
+});
+
+module.exports = router;
