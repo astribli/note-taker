f4c6e34964f18692b649681ba9d76b71b2a41657
astribli <andrewjstribling@gmail.com>
2020-10-18 17:37:29 -0500

    fix posts

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,5 +1,17 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require("path");
 const { notes } = require('../../db/db.json');
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+};
 
 router.get("/notes", (req, res) => {
     let results = notes;
@@ -9,8 +21,11 @@ router.get("/notes", (req, res) => {
 router.post("/notes", (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = notes.length.toString();
-    req.body = notes;
-    res.json(notes);
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
 });
+
+
 
 module.exports = router;
